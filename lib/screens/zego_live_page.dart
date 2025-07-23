f67bd3b6119db8451b4275f14edaf0c5c03e942b
Armel Bogue <komlanbogue@gmail.com>
2025-07-23 18:08:47 +0000

    Refactor ZegoLivePage to use StatefulWidget for live stream management and implement live ending logic for hosts

--- lib/screens/zego_live_page.dart
+++ lib/screens/zego_live_page.dart
@@ -1,7 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:zego_uikit_prebuilt_live_streaming/zego_uikit_prebuilt_live_streaming.dart';
 
-class ZegoLivePage extends StatelessWidget {
+class ZegoLivePage extends StatefulWidget {
   final String liveID;
   final String userID;
   final String userName;
@@ -16,25 +17,49 @@ class ZegoLivePage extends StatelessWidget {
   });
 
   @override
+  State<ZegoLivePage> createState() => _ZegoLivePageState();
+}
+
+class _ZegoLivePageState extends State<ZegoLivePage> {
+  Future<void> _endLiveIfHost() async {
+    if (widget.isHost) {
+      try {
+        await FirebaseFirestore.instance
+            .collection('lives')
+            .doc(widget.liveID)
+            .update({'is_live': false});
+      } catch (e) {
+        debugPrint('Erreur lors de la fin du live: $e');
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _endLiveIfHost();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: isHost
+      body: widget.isHost
           ? ZegoUIKitPrebuiltLiveStreaming(
               appID: 1145966523,
               appSign:
                   '718e87c3fe2843726ed28a6dd25197aac29eb8016d442cc84151c07b65e95d2d',
-              userID: userID,
-              userName: userName,
-              liveID: liveID,
+              userID: widget.userID,
+              userName: widget.userName,
+              liveID: widget.liveID,
               config: ZegoUIKitPrebuiltLiveStreamingConfig.host(),
             )
           : ZegoUIKitPrebuiltLiveStreaming(
               appID: 1145966523,
               appSign:
                   '718e87c3fe2843726ed28a6dd25197aac29eb8016d442cc84151c07b65e95d2d',
-              userID: userID,
-              userName: userName,
-              liveID: liveID,
+              userID: widget.userID,
+              userName: widget.userName,
+              liveID: widget.liveID,
               config: ZegoUIKitPrebuiltLiveStreamingConfig.audience(),
             ),
     );
